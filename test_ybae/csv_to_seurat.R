7eef775303f0f4c8ce1f04748e52023f01be39b5
Felipe <fsegato@stjude.org>
2023-05-04 21:34:18 +0000

    h

--- test_ybae/csv_to_seurat.R
+++ test_ybae/csv_to_seurat.R
@@ -10,33 +10,47 @@ options(future.globals.maxSize = 1e8)
 
 
 csv_to_seurat <- function(ExpressionMarker, MetaData, AssayType='Akoya', FOVName='Image') {
-  
+  # return(ExpressionMarker)
   require(Seurat);require(data.table)
   options(Seurat.object.assay.version = "v5")
+  # return("test")
+  # print("csv to seurat working")
   
+  # ExpressionMarker <- fread(ExpressionMarker, data.table = F)
+  # MetaData <- fread(MetaData)
   
-  ExpressionMarker <- fread(ExpressionMarker, data.table = F)
-  MetaData <- fread(MetaData)
+  ExpressionMarker <- read.csv(ExpressionMarker)
+  MetaData <- read.csv(MetaData)
+  # return("test")
   
   rownames(ExpressionMarker) <- ExpressionMarker[,1]; ExpressionMarker <- ExpressionMarker[,-1]
+  
   
   centroids <- data.frame(x= MetaData[,3],
                           y = MetaData[,2],
                           cell = MetaData[,1])
+  # AssayType = "Akoya"
+  # print(centroids)
+  # print(AssayType)
+  # print(head(ExpressionMarker))
+  # print(head(as.sparse(t(ExpressionMarker))))
+  # print(head(MetaData))
+  # print(FOVName)
+  # return('done')
   
   obj <- CreateSeuratObject(
     counts = as.sparse(t(ExpressionMarker)),
     assay = AssayType,
     meta.data = MetaData
   )
-  
+
   coords <- suppressWarnings(expr = CreateFOV(
     coords = centroids,
     type = 'centroids',
     key = FOVName,
     assay = AssayType
   ))
-  
+  # return("test")
   obj[[FOVName]] <- coords
   
   return(obj)
--- test_ybae/test.R
+++ test_ybae/test.R
@@ -5,14 +5,16 @@ source("csv_to_seurat.R")
 options(Seurat.object.assay.version = "v5")
 
 
-data1 <- fread("/home/lead/Akoya/BioHackathon_Samples/Sample1_Group1_MarkerExpression.csv")[1:10000]
-data2 <- fread("/home/lead/Akoya/BioHackathon_Samples/Sample1_Group1_MetaData.csv")[1:10000]
-write.csv(data1, "./test_MarkerExpression.csv")
-write.csv(data2, "./test_MeataData.csv")
-
-test <- csv_to_seurat(ExpressionMarker='/home/lead/Akoya/BioHackathon_Samples/Sample2_Group2_MarkerExpression.csv',
-                      MetaData = '/home/lead/Akoya/BioHackathon_Samples/Sample2_Group2_MetaData.csv',
-                      AssayType='Akoya', FOVName='Sample2_Group2')
+data1 <- fread("/home/lead/Akoya/BioHackathon_Samples/Sample1_Group1_MarkerExpression.csv",data.table = F)[1:10000,]
+data2 <- fread("/home/lead/Akoya/BioHackathon_Samples/Sample1_Group1_MetaData.csv", data.table=F)[1:10000,]
+write.csv(data1, "./test_MarkerExpression.csv", row.names = F)
+write.csv(data2, "./test_MeataData.csv", row.names = F)
+# 
+data1
+data2
+# test <- csv_to_seurat(ExpressionMarker="/home/lead/Akoya/BioHackathon_Samples/Sample2_Group2_MarkerExpression.csv",
+#                       MetaData = "/home/lead/Akoya/BioHackathon_Samples/Sample2_Group2_MetaData.csv",
+#                       AssayType='Akoya', FOVName='Sample2_Group2')
 
 # spare matrix
 test
