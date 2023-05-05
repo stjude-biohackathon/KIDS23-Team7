a78658f84ade90b2bbea9205e4399496a59c6d32
Felipe <fsegato@stjude.org>
2023-05-05 19:24:54 +0000

    2 more hours to go

--- test_ybae/app.R
+++ test_ybae/app.R
@@ -4,7 +4,10 @@ library(ggplot2)
 library(dplyr)  
 library(data.table)
 library(Seurat)
-source("generic_functions.R")
+library(plotly)
+source("~/KIDS2/GenericFunctions.R")
+Sys.setenv(plotly_username = "plotly_name")
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -69,11 +72,9 @@ ui <- fluidPage(
       selectInput(inputId = "assaytype",
                   label = "Assay type:",
                   choices = c("Spatial(visium)", "Akoya(phenoCycler)", "Vizgen(MERSCOPE)", "Nanostring(CosMx)"),
-                  selected = ''),
+                  selected = 'Akoya(phenoCycler)'),
       
-      
-      # selectInput("selected_feature", "Select a feature", choices = rownames(seurat_object()))
-     
+      actionButton(inputId = "run_code", label = "Run your data...",)
       
     ),
     
@@ -85,19 +86,20 @@ ui <- fluidPage(
       tableOutput("table"),
 
       tabsetPanel(
-        tabPanel("Processing",
-                 verbatimTextOutput(c("obj","obj2")),
-                 # verbatimTextOutput("obj2"),
-                 verbatimTextOutput("obj3"),
-                 verbatimTextOutput("obj4"),
-                 ),
+        # tabPanel("Processing",
+        #          verbatimTextOutput(c("obj","obj2")),
+        #          # verbatimTextOutput("obj2"),
+        #          verbatimTextOutput("obj3"),
+        #          verbatimTextOutput("obj4"),
+        #          ),
         tabPanel("QC",
           plotOutput(outputId = "qc_violin"),
           uiOutput("gene_dropdown"),
           plotOutput(outputId = "qc_histogram"),
           ),
         tabPanel("Analysis", plotOutput(outputId = "plot")),
-        tabPanel("Visualization", verbatimTextOutput(outputId = "visualization"))
+        tabPanel("Visualization",  plotlyOutput(outputId = "cell_annotation",height = 800, width = 1200)),
+        tabPanel("STAT", verbatimTextOutput(outputId = "stat"))
       )
     )
     
@@ -107,24 +109,19 @@ ui <- fluidPage(
 
 
 # Define server logic required to draw a histogram
-server <- function(input, output, session) {
+server <- function(input, output) {
   options(shiny.maxRequestSize = 1000 * 1024^2) # increase max upload size to 1000 MB
-  output$text_output <- renderText({"Instructions:\n1.choose your sample number\n2.upload your files\n3.run your QC\n4.filtering cells"})
-  
-  # using RDS
-  # output$obj <- renderPrint(
-  #   readRDS("/home/lead/Akoya/rds/Sample2_Group1_SeuratObj_sketch.rds")
-  # )
+  output$text_output <- renderText({"Instructions:\n1.choose your sample number\n2.upload your files\n3.run your data\n4.filtering cells..."})
 
   # 1. csv to Seurat object
-  seurat_object <- reactive({
-    req(input$file1)
-    req(input$file2)
-    csv_to_seurat(ExpressionMarker = input$file1$datapath, MetaData = input$file2$datapath)
-  })
-  
-  output$obj <- renderPrint({
-    print(seurat_object())})
+  # seurat_object <- reactive({
+  #   req(input$file1)
+  #   req(input$file2)
+  #   csv_to_seurat(ExpressionMarker = input$file1$datapath, MetaData = input$file2$datapath)
+  # })
+  # 
+  # output$obj <- renderPrint({
+  #   print(seurat_object())})
 
   # seurat_object <- reactive({
   #   if(input$dataset == "one sample") {
@@ -138,55 +135,89 @@ server <- function(input, output, session) {
   #   }
   # }
   
+  # output$obj <- renderPrint(
+  #   print(seurat_object())
+  # )
+  
+  # using RDS
+  seurat_object <- reactive({
+    # req(file.exists("/home/lead/Akoya/rds/Sample2_Group2_SeuratObj_sketch.rds"))
+    readRDS("/home/ybae/KIDS2/test_annoated.rds")
+  })
+  
+
+  # ui <- fluidPage(
+  #   tags$head(tags$script(src = "message-handler.js")),
+  #   actionButton("do", "Click Me")
+  # )
+  # 
+  # server <- function(input, output, session) {
+  #   observeEvent(input$do, {
+  #     session$sendCustomMessage(type = 'testmessage',
+  #                               message = 'Thank you for clicking')
+  #   })
+  # }
   
   # 2. QC # 
-  output$qc_violin = renderPlot({
+  observeEvent(input$run_code, 
+               { output$qc_violin = renderPlot({
     AssayType <- 'Akoya'
-    req(seurat_object())
-    VlnPlot(seurat_object(), features = c(paste0('nCount_', AssayType),
+    obj = seurat_object()
+    Idents(obj) = "CelltypePrediction"
+    VlnPlot(obj, features = c(paste0('nCount_', AssayType),
                                           paste0('nFeature_', AssayType)), pt.size = 0)
   })
 
   # dropdown genes
   output$gene_dropdown <- renderUI({
+    req(seurat_object())
     genes <- rownames(seurat_object())
     selectInput(inputId = "gene", label = "Select Gene", choices = genes)
   })
   
   output$qc_histogram = renderPlot({
     obj = seurat_object()
+    Idents(obj) = "CelltypePrediction"
     RidgePlot(obj, features = input$gene, ncol = 2, layer = 'counts') & xlab("")
-  })
+  })})
   
+  ### since we are using rds ### 
   # 3. normalization and sketch  # 
-  skected <- reactive({
-    norm_and_sketch(seurat_object())
-  })
+  # skected <- reactive({
+  #   norm_and_sketch(seurat_object())
+  # })
   
   # to checking
-  output$obj2 <- renderPrint({
-    print(skected())
-  })
+  # output$obj2 <- renderPrint({
+  #   print(skected())
+  # })
   
   # 4. seurat_workflow # 
-  processed_seurat <- reactive({
-    (seurat_workflow(skected()))
-  })
+  # processed_seurat <- reactive({
+  #   (seurat_workflow(skected()))
+  # })
   
   # to checking 
-  output$obj3 <- renderPrint({
-    print(processed_seurat())
-  })
+  # output$obj3 <- renderPrint({
+  #   print(processed_seurat())
+  # })
   
   # 5. annotation # 
-  annotate_seurat <- reactive({
-    (annotate_cell_types())
-  })
+  # annotate_seurat <- reactive({
+  #   (annotate_cell_types())
+  # })
   
   # to checking 
-  output$obj4 <- renderPrint({
-    print(annotate_seurat())
+  # output$obj4 <- renderPrint({
+  #   print(annotate_seurat())
+  # })
+  
+  output$cell_annotation <- renderPlotly({
+    plot <- ImageDimPlot(seurat_object(), fov = c('Sample2_Group2'), size = 0.3, group.by = 'CelltypePrediction', coord.fixed = T, dark.background = F)
+    ggplotly(plot)
+    
   })
+  
   
 
 }
--- test_ybae/test.R
+++ test_ybae/test.R
@@ -1,7 +1,7 @@
 # remotes::install_github("satijalab/seurat", "seurat5", quiet = TRUE)
 # remotes::install_github("bnprks/BPCells", quiet = TRUE)
 library(ggplot2); library(Seurat); library(dplyr)
-source("csv_to_seurat.R")
+source("~/KIDS2/GenericFunctions.R")
 options(Seurat.object.assay.version = "v5")
 
 
@@ -9,20 +9,127 @@ data1 <- fread("/home/lead/Akoya/BioHackathon_Samples/Sample1_Group1_MarkerExpre
 data2 <- fread("/home/lead/Akoya/BioHackathon_Samples/Sample1_Group1_MetaData.csv", data.table=F)[1:10000,]
 write.csv(data1, "./test_MarkerExpression.csv", row.names = F)
 write.csv(data2, "./test_MeataData.csv", row.names = F)
-# 
-data1
-data2
+
 # test <- csv_to_seurat(ExpressionMarker="/home/lead/Akoya/BioHackathon_Samples/Sample2_Group2_MarkerExpression.csv",
 #                       MetaData = "/home/lead/Akoya/BioHackathon_Samples/Sample2_Group2_MetaData.csv",
 #                       AssayType='Akoya', FOVName='Sample2_Group2')
 
 # spare matrix
-test
 test@meta.data
 test@assays$Akoya$counts
+as.data.frame(test[["Akoya"]]$counts)
+# test[["Akoya"]]$counts["DAPI",]
+
+
+# after sketch
+test <- readRDS("/home/lead/Akoya/rds/Sample2_Group2_SeuratObj_sketch.rds")
+test <- seurat_workflow(test)
+test <- annotate_cell_types(test, "/home/lead/Akoya/BioHackathon_Samples/CellType_BrCa_Akoya_Table.xlsx")
+# saveRDS(test, "/home/ybae/KIDS2/test_annoated.rds")
+test <- readRDS("/home/ybae/KIDS2/test_annoated.rds")
+
+# cell annotated plotly 
+celltypes <- unique(test$CelltypePrediction)
+plot <- ImageDimPlot(test, fov = c('Sample2_Group2'), size = 0.1, group.by = 'CelltypePrediction', coord.fixed = F, dark.background = F)
+p <- ggplotly(plot)
+
+# extract labels from ImageDimPlot
+labels_df <- test@meta.data  %>%
+  select(CelltypePrediction) %>%
+  distinct() 
+labels_df$CelltypePrediction
+plot$labels
+plot$patches
+test@
+
+celltype_colors <- Idents(test)$CelltypePredictionColors
+
+# color change 
+install.packages("paletteer")
+polychrome_pal <- DiscretePalette_scCustomize(num_colors = 36, palette = "polychrome")
+ 
+
+
+# create a separate plot with just the labels
+ggplot(labels_df, aes(x = 1, y = 10, label = CelltypePrediction)) +
+  geom_text(size = 3, color = 'black') +
+  theme_void()
+
+
+
+Idents(test) <- "CelltypePrediction"
+VlnPlot(test, features = c(paste0('nCount_', AssayType),
+                           paste0('nFeature_', AssayType)), pt.size = 0)
+
+
+
+plotly(RidgePlot(test, features = rownames(test)[1], ncol = 2, layer = 'counts'))
+plot <- RidgePlot(test, features = rownames(test)[1], ncol = 2, layer = 'counts')
+plot = ggplotly(plot)
+plot
+
+library(ggridges)
+# create example data
+df <- data.frame(
+  gene = c(rep("gene1", 100), rep("gene2", 100), rep("gene3", 100)),
+  sample = rep(1:100, 3),
+  expression = c(rnorm(100, mean = 5, sd = 1), rnorm(100, mean = 10, sd = 1), rnorm(100, mean = 15, sd = 1))
+)
+df$expression
+ggplot(df, aes(x = expression, y = gene, fill = gene)) +
+  geom_density_ridges(alpha = 0.8) +
+  theme_ridges() +
+  xlab("Expression")
+# ggplot(test@assays$Akoya@data, aes(x = test@assays$Akoya@data[, "Feature1"], y = rownames(test@assays$Akoya@data))) +
+#   geom_density_ridges(alpha = 0.8, scale = 1.5) +
+#   theme_ridges() +
+#   xlab("Expression")
+
+# plot the ridgeline
+typeof(as.data.frame(test[["Akoya"]]$counts))
+head(as.data.frame(test[["Akoya"]]$counts))
+rownames(test[["Akoya"]]$counts)
+
+test[["Akoya"]]$counts["DAPI",]
+
+ggplot(test[["Akoya"]]$counts, aes(x ="DAPI", y = test[["Akoya"]]$counts["DAPI",])) +
+  geom_density_ridges(alpha = 0.8) +
+  theme_ridges() 
+
+test[["Akoya"]]$counts["DAPI",]
 test[["Akoya"]]$counts
 
-RidgePlot(test, features = rownames(test)[1:10], ncol = 2, layer = 'counts') & xlab("")
+
+# Convert the sparse matrix to a dense matrix
+counts_matrix <- as.matrix(test[["Akoya"]]$counts)
+head(as.data.frame(counts_matrix))
+show(counts_matrix)
+# Create the ggplot
+df <- as.data.frame(counts_matrix)
+df_long <- melt(df)
+dim(df)
+df[1,]
+
+df_long[1,]
+dim(df_long)
+ggplot(data = df_long, aes(x ="DAPI", y = values)) +
+  geom_density_ridges(alpha = 0.8) +
+  theme_ridges()
+
+
+## new 
+# Convert the dgMatrix to a data.frame
+counts_df <- as.data.frame(as.matrix(test[["Akoya"]]$counts))
+rownames(counts_df) <- rownames(test)
+colnames(counts_df) <- colnames(test)
+head(counts_df)
+
+rownames(counts_df)
+colnames(counts_df)
+# Extract the DAPI column as a vector
+dapi <- as.numeric(counts_df["DAPI"])
+
+
 VlnPlot(test, features="nCount_Akoya", layer='counts')
 test[[]]
 Layers(test)
@@ -30,3 +137,6 @@ Layers(test)
 
 Sample1_Group1_SeuratObj_sketch <- readRDS("./test_yj/Sample1_Group1_SeuratObj_sketch.rds")
 VlnPlot(Sample1_Group1_SeuratObj_sketch, features="nCount_Akoya", layer='counts')
+
+
+
